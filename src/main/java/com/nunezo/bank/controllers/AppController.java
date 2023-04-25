9cbe5bb5d575b3681ee6a1aa571d81197a75e875
unknown <nunezfrancisco400@gmail.com>
2023-04-24 20:29:54 -0400

    the very begining

---
+++ src/main/java/com/nunezo/bank/controllers/AppController.java
@@ -0,0 +1,13 @@
+package com.nunezo.bank.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("")
+    public String indexDisplay(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/nunezo/bank/models/data/User.java
@@ -0,0 +1,52 @@
+package com.nunezo.bank.models.data;
+
+//@Entity
+//@Table(name = "users")
+public class User {
+
+
+    private int id;
+
+
+    private String name;
+
+
+    private String lastName;
+
+
+    private String email;
+
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
